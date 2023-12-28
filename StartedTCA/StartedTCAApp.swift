eafdca2bfc153c1dd0eb9e0d9e638e9e67674a68
cuong <cuongnt04230@gmail.com>
2023-12-28 13:10:25 +0700

    Initial Commit

---
+++ StartedTCA/StartedTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartedTCAApp.swift
+//  StartedTCA
+//
+//  Created by Cuong Nguyen on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct StartedTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StartedTCAUITests/StartedTCAUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StartedTCAUITestsLaunchTests.swift
+//  StartedTCAUITests
+//
+//  Created by Cuong Nguyen on 12/28/23.
+//
+
+import XCTest
+
+final class StartedTCAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
