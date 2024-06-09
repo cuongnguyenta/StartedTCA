04ef6ed41924352ea0008fdb51904eed8a343641
cuong <cuongnt04230@gmail.com>
2024-06-09 18:12:47 +0700

    Unit tests state change

---
+++ StartedTCATests/CounterFeatureTests.swift
@@ -0,0 +1,28 @@
+//
+//  CounterFeatureTests.swift
+//  StartedTCATests
+//
+//  Created by Cuong Nguyen on 6/9/24.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import StartedTCA
+
+@MainActor
+final class CounterFeatureTests: XCTestCase {
+    func testCounter() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.incrementButtonTapped) {
+            $0.count = 1
+        }
+        
+        await store.send(.decrementButtonTapped) {
+            $0.count = 0
+        }
+    }
+}
