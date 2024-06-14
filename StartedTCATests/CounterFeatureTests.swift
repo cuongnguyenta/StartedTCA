35361dc94bbf65e0862441ab6ef4cdaf1e179804
cuong <cuongnt04230@gmail.com>
2024-06-14 14:14:13 +0700

    Add unit tests

--- StartedTCATests/CounterFeatureTests.swift
+++ StartedTCATests/CounterFeatureTests.swift
@@ -25,4 +25,22 @@ final class CounterFeatureTests: XCTestCase {
             $0.count = 0
         }
     }
+    
+    func testTimer() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.timerButtonTapped) {
+            $0.isTimerRunning = true
+        }
+        
+        await store.receive(\.timerTick) {
+            $0.count = 1
+        }
+        
+        await store.send(.timerButtonTapped) {
+            $0.isTimerRunning = false
+        }
+    }
 }
